bea9ffa981135efe440f6f3f60161a161af39b0a
Jacobokia <cobs399@gmail.com>
2018-08-14 14:08:52 +0300

    test the server and check if it is running on localhost

--- app.js
+++ app.js
@@ -6,5 +6,16 @@ var path = require('path')
 
 var app = express();
 
+const route = require('./routes/route');
+
 //port no.
-const port = 4000;
+const port = 3000;
+
+//testing a server
+app.get('/',function(req, res){
+  res.send('server is working')
+});
+
+app.listen(3000,function(){
+  console.log('server is running on port');
+})
