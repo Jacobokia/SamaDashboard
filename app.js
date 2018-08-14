441d61670a42084cde5e431db74117629550a2f3
Jacobokia <cobs399@gmail.com>
2018-08-14 12:23:46 +0300

    create variables for express, mongoose,cors, body-parser and path

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var bodyparser = require('body-parser');
+var path = require('path')
+
+var app = express();
